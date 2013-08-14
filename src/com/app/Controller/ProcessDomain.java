7bc120c2e1bf632ffb6ab748b8885427e37691c4
Redeagle48 <antoniopereira350@sapo.pt>
2013-08-14 22:08:50 +0100

    - The arrays are sorting

--- src/com/app/Controller/ProcessDomain.java
+++ src/com/app/Controller/ProcessDomain.java
@@ -52,21 +52,29 @@ public class ProcessDomain {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
+
 			ArrayList<Slot> slot_array;
 			if(!seasons.containsKey(month)) {
 				slot_array = new ArrayList<Slot>();
 			} else {
 				slot_array = seasons.get(month);
 			}
-			
+
 			Slot slot = new Slot(season,date_initString,date_endString);
-			slot_array.add(slot);
+			try {
+				slot_array = AddSlot(slot_array, slot);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//slot_array.add(slot);
+
 			seasons.put(month, slot_array);
 		}
-		
+
 		if(com.app.domain.Values.debugDomain){
 			Log.i("ExecuteDomain","===========HASHMAP============");
+			Log.i("ExecuteDomain","===========ORGANIZED============");
 			Log.i("ExecuteDomain", "Tamanho: " + seasons.size());
 			Set<Integer> set = seasons.keySet();
 			for (Integer i : set) {
@@ -80,7 +88,39 @@ public class ProcessDomain {
 	}
 
 	//TODO
-	public void organize() {
-		
+	public ArrayList<Slot> AddSlot(ArrayList<Slot> slot_array, Slot slot) throws ParseException {
+
+		Log.i("Organize","=========COMECA=============");
+		Log.i("Organize","tamanho do array "+slot_array.size());
+
+		ArrayList<Slot> slot_temp = new ArrayList<Slot>();
+		if(slot_array.size() == 0) {
+			slot_temp.add(slot);
+			Log.i("Organize","Acao para o array vazio");
+		}
+		else {
+			Log.i("Organize","Acao para o array com elementos");
+
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+			Date slot_date = sdf.parse(slot.getSlotBeginDate());
+			boolean done = false;
+			for(int i = 0; i < slot_array.size(); i++) {
+				Slot slotItem = slot_array.get(i);
+				Date slotItem_date = sdf.parse(slotItem.getSlotBeginDate());
+				if(slot_date.before(slotItem_date) && !done) {
+					slot_temp.add(slot);
+					slot_temp.add(slotItem);
+					done = true;
+				} else {
+					slot_temp.add(slotItem);
+				}
+
+				if(i == slot_array.size()-1 && !done) {
+					slot_temp.add(slot);
+					done = true;
+				}
+			}
+		}
+		return slot_temp;
 	}
 }
